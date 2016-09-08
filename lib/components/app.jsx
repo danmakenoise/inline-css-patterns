fa5a28b0190e0ee74140a8448f80f1166927ca6c
Dan Phillips <dphillips@mic.com>
2016-09-07 23:12:57 -0400

    basic styling added to app

--- lib/components/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div className='app'>
-    <h1>Test CSS Page</h1>
-  </div>
-);
-
-export default App;
---
+++ lib/components/app/index.jsx
@@ -0,0 +1,14 @@
+import { css } from 'aphrodite';
+import React from 'react';
+
+import baseStyles from '../../styles';
+
+const App = () => (
+  <div className={}>
+    <h1 className={css(baseStyles.h1)}>
+      Test CSS Page
+    </h1>
+  </div>
+);
+
+export default App;
